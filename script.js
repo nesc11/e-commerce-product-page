e9bbfb323337e31a98820716383a9ca0125528b0
nescpro <salazarnestor1996@gmail.com>
2024-03-11 16:07:57 -0500

    Implement carousel styles

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const iconMenuContainer  = document.querySelector('.icon-menu-container')
     const navLinks = document.querySelector('.nav-links')
+
+    const carouselBtnPrevious = document.querySelector('.previous')
+    const carouselBtnNext = document.querySelector('.next')
+
     iconMenuContainer.addEventListener('click', () => {
         iconMenuContainer.classList.toggle('expanded')
         navLinks.classList.toggle('expanded')
+    })
+
+    carouselBtnNext.addEventListener('click', () => {
+        const currentSlide = document.querySelector('.current-slide')
+        const nextSibling = currentSlide.nextElementSibling
+        if (!nextSibling) return
+        nextSibling.classList.add('current-slide')
+        currentSlide.classList.remove('current-slide')
+    })
+
+    carouselBtnPrevious.addEventListener('click', () => {
+        const currentSlide = document.querySelector('.current-slide')
+        const prevSibling = currentSlide.previousElementSibling
+        if (!prevSibling) return
+        prevSibling.classList.add('current-slide')
+        currentSlide.classList.remove('current-slide')
     })
 })
